0a9860d5ef3969d5b9b3ef9b9dfc932e7ba184ed
Daniel Vrátil <dvratil@kde.org>
2017-09-11 14:32:11 +0200

    Adapt to KCalCore recurrenceId changes

--- src/itiphandler.cpp
+++ src/itiphandler.cpp
@@ -377,7 +377,7 @@ void ITIPHandler::sendAsICalendar(const KCalCore::Incidence::Ptr &originalIncide
 
         if (incidence->hasRecurrenceId()) {
             // For an individual occurrence, recur id doesn't make sense, since we're not sending the whole recurrence series.
-            incidence->setRecurrenceId(KDateTime());
+            incidence->setRecurrenceId({});
         }
 
         KCalCore::ICalFormat format;
