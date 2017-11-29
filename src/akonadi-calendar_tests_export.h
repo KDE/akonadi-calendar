d0c76ea9697c5ff50bc51b742d2ae626d6e1225c
Montel Laurent <montel@kde.org>
2017-11-29 07:24:40 +0100

    Use same method as other repo

---
+++ src/akonadi-calendar_tests_export.h
@@ -0,0 +1,16 @@
+
+#ifndef AKONADICALENDARTEST_EXPORT_H
+#define AKONADICALENDARTEST_EXPORT_H
+
+#include "akonadi-calendar_export.h"
+
+/* Classes which are exported only for unit tests */
+#ifdef BUILD_TESTING
+# ifndef AKONADI_CALENDAR_TESTS_EXPORT
+#  define AKONADI_CALENDAR_TESTS_EXPORT AKONADI_CALENDAR_EXPORT
+# endif
+#else /* not compiling tests */
+# define AKONADI_CALENDAR_TESTS_EXPORT
+#endif
+
+#endif
