45d4577dde571d51291bb215c52c0e39eaf68fba
Laurent Montel <montel@kde.org>
2019-08-26 07:43:24 +0200

    Use QLatin1String in join/startswith/endsWith/contains + QString::replace operates on the same object, no need to reassign

--- src/mailclient_p.cpp
+++ src/mailclient_p.cpp
@@ -103,11 +103,11 @@ void MailClient::mailAttendees(const KCalendarCore::IncidenceBase::Ptr &incidenc
     }
     QString to;
     if (!toList.isEmpty()) {
-        to = toList.join(QStringLiteral(", "));
+        to = toList.join(QLatin1String(", "));
     }
     QString cc;
     if (!ccList.isEmpty()) {
-        cc = ccList.join(QStringLiteral(", "));
+        cc = ccList.join(QLatin1String(", "));
     }
 
     QString subject;
