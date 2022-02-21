a446b69d0f61adc32492b5510fd878eb793b6765
Laurent Montel <montel@kde.org>
2022-02-21 12:56:15 +0100

    Use new akonadi calendar install include

--- examples/etmcalendar/main.cpp
+++ examples/etmcalendar/main.cpp
@@ -4,7 +4,7 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
-#include <Akonadi/Calendar/ETMCalendar>
+#include <Akonadi/ETMCalendar>
 #include <KCheckableProxyModel>
 
 #include <QApplication>
