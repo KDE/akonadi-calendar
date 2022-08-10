KDE/akonadi-calendar

commit d82e7001caa0e2a391e46ee5c26f568b015e9b72

Use the new KCalendarCore::Calendar loading state API
The old Akonadi-specific isLoaded() method in CalendarBase can then be
dropped once all consumers have been adjusted.

src/fetchjobcalendar.cpp
@@ -18,6 +18,7 @@ FetchJobCalendarPrivate::FetchJobCalendarPrivate(FetchJobCalendar *qq)
     : CalendarBasePrivate(qq)
     , q(qq)
 {
+    q->setIsLoading(true);
     auto job = new IncidenceFetchJob();
     connect(job, &KJob::result, this, &FetchJobCalendarPrivate::slotSearchJobFinished);
     connect(this, &CalendarBasePrivate::fetchFinished, this, &FetchJobCalendarPrivate::slotFetchJobFinished);
@@ -54,7 +55,7 @@ void FetchJobCalendarPrivate::slotSearchJobFinished(KJob *job)
 
 void FetchJobCalendarPrivate::slotFetchJobFinished()
 {
-    m_isLoaded = true;
+    q->setIsLoading(false);
     // Q_EMIT loadFinished() in a delayed manner, due to freezes because of execs.
     QMetaObject::invokeMethod(q, "loadFinished", Qt::QueuedConnection, Q_ARG(bool, m_success), Q_ARG(QString, m_errorMessage));
 }
@@ -65,12 +66,6 @@ FetchJobCalendar::FetchJobCalendar(QObject *parent)
 }
 
 FetchJobCalendar::~FetchJobCalendar() = default;
-
-bool FetchJobCalendar::isLoaded() const
-{
-    auto d = static_cast<FetchJobCalendarPrivate *>(d_ptr.get());
-    return d->m_isLoaded;
-}
 
 #include "moc_fetchjobcalendar.cpp"
 #include "moc_fetchjobcalendar_p.cpp"

autotests/etmcalendartest.cpp
@@ -90,7 +90,7 @@ void ETMCalendarTest::initTestCase()
     fetchCollection();
 
     mCalendar = new ETMCalendar();
-    QVERIFY(!mCalendar->isLoaded());
+    QVERIFY(mCalendar->isLoading());
     connect(mCalendar, &ETMCalendar::collectionsAdded, this, &ETMCalendarTest::handleCollectionsAdded);
 
     mCalendar->registerObserver(this);
@@ -115,7 +115,7 @@ void ETMCalendarTest::initTestCase()
     // Wait for incidences
     QTestEventLoop::instance().enterLoop(10);
     QVERIFY(!QTestEventLoop::instance().timeout());
-    QVERIFY(mCalendar->isLoaded());
+    QVERIFY(!mCalendar->isLoading());
 }
 
 void ETMCalendarTest::cleanupTestCase()
@@ -247,7 +247,7 @@ void ETMCalendarTest::calendarIncidenceAdded(const Incidence::Ptr &incidence)
 
 void ETMCalendarTest::handleCollectionsAdded(const Akonadi::Collection::List &)
 {
-    QVERIFY(!mCalendar->isLoaded());
+    QVERIFY(mCalendar->isLoading());
     QTestEventLoop::instance().exitLoop();
 }
 

calendar-plugin/singlecollectioncalendar.cpp
@@ -12,8 +12,6 @@
 #include <Akonadi/ItemFetchScope>
 #include <Akonadi/Monitor>
 
-#include <kcalendarcore_version.h>
-
 SingleCollectionCalendar::SingleCollectionCalendar(const Akonadi::Collection &col, QObject *parent)
     : Akonadi::CalendarBase(parent)
 {
@@ -22,10 +20,8 @@ SingleCollectionCalendar::SingleCollectionCalendar(const Akonadi::Collection &co
     incidenceChanger()->setDefaultCollection(col);
     incidenceChanger()->setGroupwareCommunication(false);
     incidenceChanger()->setDestinationPolicy(Akonadi::IncidenceChanger::DestinationPolicyNeverAsk);
-
-#if KCALENDARCORE_VERSION >= QT_VERSION_CHECK(5, 96, 0)
     setIsLoading(true);
-#endif
+
     // here and below for monitoring: ensure items have a parent collection
     // before calling internalInsert to prevent an extra collection fetch job
     auto job = new Akonadi::ItemFetchJob(col, this);
@@ -37,9 +33,7 @@ SingleCollectionCalendar::SingleCollectionCalendar(const Akonadi::Collection &co
             item.setParentCollection(m_collection);
             d->internalInsert(item);
         }
-#if KCALENDARCORE_VERSION >= QT_VERSION_CHECK(5, 96, 0)
         setIsLoading(false);
-#endif
     });
 
     auto monitor = new Akonadi::Monitor(this);

src/calendarbase.cpp
@@ -652,7 +652,7 @@ void CalendarBase::endBatchAdding()
 
 bool CalendarBase::isLoaded() const
 {
-    return true;
+    return !isLoading();
 }
 
 #include "moc_calendarbase.cpp"

src/calendarbase.h
@@ -195,7 +195,7 @@ public:
      * Returns if the calendar already finished loading.
      * Base class returns true.
      */
-    virtual bool isLoaded() const;
+    [[deprecated("use isLoading() instead")]] bool isLoaded() const;
 
 Q_SIGNALS:
     /**

src/etmcalendar.cpp
@@ -94,6 +94,7 @@ void ETMCalendarPrivate::init()
     connect(mFilteredETM, &QAbstractItemModel::rowsAboutToBeRemoved, this, &ETMCalendarPrivate::onRowsAboutToBeRemovedInFilteredModel);
 
     loadFromETM();
+    updateLoadingState();
 }
 
 void ETMCalendarPrivate::onCollectionChanged(const Akonadi::Collection &collection, const QSet<QByteArray> &attributeNames)
@@ -298,6 +299,7 @@ void ETMCalendarPrivate::onCollectionPopulated(Akonadi::Collection::Id id)
 {
     mPopulatedCollectionIds.insert(id);
     Q_EMIT q->calendarChanged();
+    updateLoadingState();
 }
 
 void ETMCalendarPrivate::onRowsRemoved(const QModelIndex &index, int start, int end)
@@ -576,21 +578,19 @@ bool ETMCalendar::collectionFilteringEnabled() const
     return d->mCollectionFilteringEnabled;
 }
 
-bool ETMCalendar::isLoaded() const
+void ETMCalendarPrivate::updateLoadingState()
 {
-    Q_D(const ETMCalendar);
-
-    if (!entityTreeModel()->isCollectionTreeFetched()) {
-        return false;
+    if (!q->entityTreeModel()->isCollectionTreeFetched()) {
+        return q->setIsLoading(true);
     }
 
-    for (const Akonadi::Collection &collection : std::as_const(d->mCollectionMap)) {
-        if (!entityTreeModel()->isCollectionPopulated(collection.id())) {
-            return false;
+    for (const Akonadi::Collection &collection : std::as_const(mCollectionMap)) {
+        if (!q->entityTreeModel()->isCollectionPopulated(collection.id())) {
+            return q->setIsLoading(true);
         }
     }
 
-    return true;
+    q->setIsLoading(false);
 }
 
 #include "moc_etmcalendar.cpp"

src/etmcalendar.h
@@ -154,11 +154,6 @@ public:
      */
     Q_REQUIRED_RESULT bool collectionFilteringEnabled() const;
 
-    /**
-     * Returns if the calendar already finished loading.
-     */
-    Q_REQUIRED_RESULT bool isLoaded() const override;
-
 Q_SIGNALS:
     /**
      * This signal is emitted if a collection has been changed (properties or attributes).

src/etmcalendar_p.h
@@ -104,6 +104,8 @@ public Q_SLOTS:
     void onCollectionChanged(const Akonadi::Collection &collection, const QSet<QByteArray> &attributeNames);
     void onCollectionPopulated(Akonadi::Collection::Id);
 
+    void updateLoadingState();
+
 public:
     Akonadi::CalendarModel::Ptr mETM;
     Akonadi::EntityMimeTypeFilterModel *mFilteredETM = nullptr;

commit b4abd2d3018106181e0ebc6f1614ad457e257576

Fix crash when opening e-mail with calendar invitation
Commit d82e7001 introduced use of new loading state API, which has
unfortunately caused a regression. The reason for it is a constructor
circular dependency. The FetchJobCalendarPrivate class constructor is
trying to call a method of a base class, which hasn't been constructed
at this point, as the CalendarBase constructor has not executed yet
waiting for the FetchJobCalendarPrivate constructor to finish.

Untangle this mess by moving the method call to the FetchJobCalendar
constructor, at which point the base class is already initialized.

Signed-off-by: Krzysztof Nowicki <krissn@op.pl>

src/fetchjobcalendar.cpp
@@ -18,7 +18,6 @@ FetchJobCalendarPrivate::FetchJobCalendarPrivate(FetchJobCalendar *qq)
     : CalendarBasePrivate(qq)
     , q(qq)
 {
-    q->setIsLoading(true);
     auto job = new IncidenceFetchJob();
     connect(job, &KJob::result, this, &FetchJobCalendarPrivate::slotSearchJobFinished);
     connect(this, &CalendarBasePrivate::fetchFinished, this, &FetchJobCalendarPrivate::slotFetchJobFinished);
@@ -63,6 +62,7 @@ void FetchJobCalendarPrivate::slotFetchJobFinished()
 FetchJobCalendar::FetchJobCalendar(QObject *parent)
     : CalendarBase(new FetchJobCalendarPrivate(this), parent)
 {
+    setIsLoading(true);
 }
 
 FetchJobCalendar::~FetchJobCalendar() = default;