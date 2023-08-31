59dca599a67c3e65bd762aa0f202f94500490280
Claudio Cambra <claudio.cambra@kde.org>
2023-08-31 08:17:11 +0000

    Do not Q_DECLARE_METATYPE for Identity as this is now done in KIdentityManagement

--- autotests/mailclienttest.cpp
+++ autotests/mailclienttest.cpp
@@ -44,7 +44,6 @@ using ContactPreferences = QMap<QString, MessageComposer::ContactPreference>;
 
 using namespace Akonadi;
 
-Q_DECLARE_METATYPE(KIdentityManagement::Identity)
 Q_DECLARE_METATYPE(KCalendarCore::Incidence::Ptr)
 Q_DECLARE_METATYPE(CryptoState)
 Q_DECLARE_METATYPE(QVector<ExpectedDialog>)
