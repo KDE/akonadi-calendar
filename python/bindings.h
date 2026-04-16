5d9e17ac77e60bb92282e114cc637e22da615e3e
Alan Thouvenin <alan.thouvenin@enioka.com>
2026-04-16 17:11:17 +0200

    Add python bindings

---
+++ python/bindings.h
@@ -0,0 +1,32 @@
+/*
+    SPDX-FileCopyrightText: 2026 Alan Thouvenin <alan.thouvenin@enioka.com>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <QItemSelection>
+
+#include <Akonadi/BlockAlarmsAttribute>
+#include <Akonadi/CalFilterPartStatusProxyModel>
+#include <Akonadi/CalFilterProxyModel>
+#include <Akonadi/CalendarBase>
+#include <Akonadi/CalendarClipboard>
+#include <Akonadi/CalendarUtils>
+#include <Akonadi/CollectionCalendar>
+#include <Akonadi/ETMCalendar>
+#include <Akonadi/FetchJobCalendar>
+#include <Akonadi/FreeBusyManager>
+#include <Akonadi/History>
+#include <Akonadi/ICalImporter>
+#include <Akonadi/ITIPHandler>
+#include <Akonadi/IncidenceChanger>
+#include <Akonadi/IncidenceTreeModel>
+#include <Akonadi/Monitor>
+#include <Akonadi/PublishDialog>
+#include <Akonadi/SearchCollectionHelper>
+#include <Akonadi/StandardActionManager>
+#include <Akonadi/StandardCalendarActionManager>
+#include <Akonadi/TodoModel>
+#include <Akonadi/TodoPurger>
